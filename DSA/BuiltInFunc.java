9438649e22d5c8fcd19faf86819d37082e90ca00
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-18 12:02:02 +0530

    Add BuiltInFunc class with basic mathematical function demonstrations

---
+++ DSA/BuiltInFunc.java
@@ -0,0 +1,11 @@
+public class BuiltInFunc {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(16));  // Output: 4.0
+        System.out.println(Math.pow(2, 3)); // Output: 8.0
+        System.out.println(Math.cbrt(100));
+        System.out.println(Math.floor(-7.4));
+        System.out.println(Math.ceil(7.4));
+        System.out.println(Math.min(3, 9));
+        
+    }
+}
