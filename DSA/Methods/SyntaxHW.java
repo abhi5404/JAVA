c7faeda814f526b2c5c1643d1dcfbd031e35b9b5
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-18 12:52:34 +0530

    Add SyntaxHW class with methods for recursive calls and printing names

---
+++ DSA/Methods/SyntaxHW.java
@@ -0,0 +1,20 @@
+package Methods;
+
+public class SyntaxHW {
+    public static void shravan (){
+        karan();
+        System.out.println("khushi");
+    }
+    public static void main(String[] args) {
+        riyanshi();
+    }
+    public static void karan() {
+        System.out.println("YASHIKA");
+    }
+    public static void riyanshi() {
+        karan();
+        shravan();
+        System.out.println("Ajay");
+        shravan();
+    }
+}
