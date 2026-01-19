cdb61b78deda346c377d52532f08ea430719a89b
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-19 20:18:40 +0530

    Refactor outputInputArrays class to accept user input for array size and elements

--- DSA/Arrays/outputInputArrays.java
+++ DSA/Arrays/outputInputArrays.java
@@ -1,10 +1,22 @@
 package Arrays;
+import java.util.Scanner;
 
 public class outputInputArrays {
     public static void main(String[] args) {
-        int [] arr = {5, 10, 15, 20, 25};
-        for(int i = 0; i < arr.length; i++) {
+        // int [] arr = {5, 10, 15, 20, 25};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        System.out.println("Enter " + size + " elements for the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        System.out.println("The elements in the array are:");
+        for (int i = 0; i < size; i++) {
             System.out.println(arr[i]);
         }
     }
 }
+
