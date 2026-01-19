884e4fe6bdb02cf89a78edd9917dab6d75e9b7ad
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-19 20:30:27 +0530

    Add SumOfArraysElement class for calculating the sum of array elements

---
+++ DSA/Arrays/SumOfArraysElement.java
@@ -0,0 +1,23 @@
+package Arrays;
+import java.util.Scanner;
+
+public class SumOfArraysElement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size=sc.nextInt();
+        int [] arr=new int[size];
+        System.out.println("Enter " + size + " elements for the array:");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<size;i++){
+            sum+=arr[i];
+        }
+        System.out.println("The sum of the array elements is: " + sum);
+        
+    }
+    
+    
+}
