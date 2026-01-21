e575fb20d26a054ab56cf5c6a9ede67e53500a02
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-22 03:17:26 +0530

    Add RotateArrays class to rotate elements of an array

---
+++ DSA/Arrays/RotateArrays.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class RotateArrays {
+static void rotate(int []arr, int d){
+    int n=arr.length;
+    d=d%n; //in case d>n
+    reverse(arr,0,d-1);
+    reverse(arr,d,n-1);
+    reverse(arr,0,n-1);
+}
+
+static void reverse(int []arr, int start, int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+        public static void main(String[] args) {
+            int []arr= {1,2,3,4,5,6,7};
+            int d=3;
+            rotate(arr,d);
+            for(int ele : arr) System.out.print(ele+" ");
+        }
+    }
