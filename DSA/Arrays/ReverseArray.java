cb58180bbb33a700adb5a3f2575f8ee3f4b2c3b1
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-21 20:16:21 +0530

    Refactor ReverseArray to use a for loop for reversing elements

--- DSA/Arrays/ReverseArray.java
+++ DSA/Arrays/ReverseArray.java
@@ -1,16 +1,34 @@
+// package Arrays;
+
+// public class ReverseArray {
+//     public static void main(String[] args) {
+//         int []arr= {12,34,65,90,2,5,98};
+//         int i=0;
+//         int j=arr.length-1;
+//         while(i<j){
+//             int temp=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//             i++;
+//             j--;
+//         }
+//         for(int ele : arr) System.out.print(ele+" ");
+//     }
+    
+// }
+
+// using for loop
+
 package Arrays;
 
 public class ReverseArray {
     public static void main(String[] args) {
         int []arr= {12,34,65,90,2,5,98};
-        int i=0;
-        int j=arr.length-1;
-        while(i<j){
+        int n=arr.length;
+        for(int i=0;i<n/2;i++){
             int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
+            arr[i]=arr[n-1-i];
+            arr[n-1-i]=temp;
         }
         for(int ele : arr) System.out.print(ele+" ");
     }
