fc6ca410c90c6d97cb29d5b34533cbc2970f8d29
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-26 02:11:03 +0530

    Restore original implementation of DuplicateElement class and enhance duplicate detection logic

--- DSA/Arrays/DuplicateElement.java
+++ DSA/Arrays/DuplicateElement.java
@@ -1,36 +1,36 @@
 
 
-// package Arrays;
+package Arrays;
 
-// public class DuplicateElement {
-//     public static void main(String[] args) {
-//         int arr[] = {1, 2, 3, 4, 2, 5, 3};
-//         int n = arr.length;
-//         System.out.print("Duplicate Elements: ");
-//         for (int i = 0; i < n; i++) {
-//             for (int j = i + 1; j < n; j++) {
-//                 if (arr[i] == arr[j]) {
-//                     System.out.print(arr[i] + " ");
-//                     break; // To avoid printing the same duplicate multiple times
-//                 }
-//             }
-//         }
-//     }
-// }
+public class DuplicateElement {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 2, 5, 3};
+        int n = arr.length;
+        System.out.print("Duplicate Elements: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                    break; // To avoid printing the same duplicate multiple times
+                }
+            }
+        }
+    }
+}
 
 
 // Using Sum Approach
-package Arrays;
-public class DuplicateElement {
-    public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 2, 5};
-        int n = arr.length - 1; // Since one number is duplicate
-        int expectedSum = n * (n + 1) / 2;
-        int actualSum = 0;
-        for (int ele : arr) {
-            actualSum += ele;
-        }
-        int duplicateNumber = actualSum - expectedSum;
-        System.out.println("The duplicate number is: " + duplicateNumber);
-    }
-}
+// package Arrays;
+// public class DuplicateElement {
+//     public static void main(String[] args) {
+//         int arr[] = {1, 2, 3, 4, 2, 5};
+//         int n = arr.length - 1; // Since one number is duplicate
+//         int expectedSum = n * (n + 1) / 2;
+//         int actualSum = 0;
+//         for (int ele : arr) {
+//             actualSum += ele;
+//         }
+//         int duplicateNumber = actualSum - expectedSum;
+//         System.out.println("The duplicate number is: " + duplicateNumber);
+//     }
+// }
