b32dbad5e0dabb45241529385283b0cc3f1224ca
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-26 02:09:46 +0530

    Add commented-out alternative implementation for DuplicateElement using Sum Approach

--- DSA/Arrays/DuplicateElement.java
+++ DSA/Arrays/DuplicateElement.java
@@ -17,3 +17,20 @@ public class DuplicateElement {
         }
     }
 }
+
+
+// Using Sum Approach
+// package Arrays;
+// public class DuplicateElement {
+//     public static void main(String[] args) {
+//         int arr[] = {1, 2, 3, 4, 2, 5};
+//         int n = arr.length - 1; // Since one number is duplicate
+//         int expectedSum = n * (n + 1) / 2;
+//         int actualSum = 0;
+//         for (int ele : arr) {
+//             actualSum += ele;
+//         }
+//         int duplicateNumber = actualSum - expectedSum;
+//         System.out.println("The duplicate number is: " + duplicateNumber);
+//     }
+// }
