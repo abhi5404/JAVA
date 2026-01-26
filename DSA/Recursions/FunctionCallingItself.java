16710deed140de0ece942faff4d461c1eba37f32
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-26 10:53:21 +0530

    Refactor FunctionCallingItself.java to implement countdown recursion and update main method

--- DSA/Recursions/FunctionCallingItself.java
+++ DSA/Recursions/FunctionCallingItself.java
@@ -1,13 +1,27 @@
+// package Recursions;
+
+// public class FunctionCallingItself {
+//     public static void main(String[] args) {
+//         ajay(1);
+//     }
+
+//     public static void ajay (int n )
+// {
+//     if (n==10) return ;
+//     System.out.println("priya");
+//     ajay(n+1);
+// }}
+
 package Recursions;
 
 public class FunctionCallingItself {
     public static void main(String[] args) {
-        ajay(1);
+        print(5);
     }
 
-    public static void ajay (int n )
+    public static void print (int n )
 {
-    if (n==10) return ;
-    System.out.println("priya");
-    ajay(n+1);
+    if (n==0) return ;
+    System.out.println(n);
+     print(n-1);
 }}
