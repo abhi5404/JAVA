85f69347b6fb405cdbd0efdfdb11f74bdfe07834
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-27 00:50:26 +0530

    Add newline at end of file in DecreasingIncreasing.java

---
+++ DSA/Recursions/Factorial.java
@@ -0,0 +1,18 @@
+package Recursions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.print("Enter N : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Factorial: " + fact(n));
+        sc.close();
+    }
+    public static int fact (int n){
+        if (n==0 || n==1) return 1;
+        int ans = n*fact(n-1);
+        return ans;
+    }
+}
