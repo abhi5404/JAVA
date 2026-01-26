bb50548d5e1d4a61263288790bb7a8da132ad7c1
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-27 02:55:14 +0530

    Refactor ReverseOfANumber.java to simplify reverse logic and improve readability

--- DSA/Recursions/ReverseOfANumber.java
+++ DSA/Recursions/ReverseOfANumber.java
@@ -3,17 +3,24 @@ package Recursions;
 import java.util.Scanner;
 
 public class ReverseOfANumber {
+    static int rev = 0;
+
     public static void main(String[] args) {
-        System.out.print("Enter N : ");
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
         int n = sc.nextInt();
-        int digits = (int)Math.log10(n) + 1; // Calculate number of digits
-        System.out.println("Reverse: " + reverse(n, digits));
+
+        reverse(n);
+        System.out.println("Reverse: " + rev);
+
         sc.close();
     }
-    public static int reverse(int n, int digits){
-        if (n % 10 == n) return n;
-        int rem = n % 10;
-        return rem * (int)Math.pow(10, digits - 1) + reverse(n / 10, digits - 1);
+
+    static void reverse(int n) {
+        if (n == 0) return;
+
+        rev = rev * 10 + (n % 10);
+        reverse(n / 10);
     }
 }
+
