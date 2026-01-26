5f8fd02206cccdbcfdd121fe0cb905d88aace60b
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-26 11:32:08 +0530

    Add Print1toN.java to demonstrate printing numbers from 1 to N using recursion

---
+++ DSA/Recursions/Print1toN.java
@@ -0,0 +1,19 @@
+package Recursions;
+
+import java.util.Scanner;
+
+public class Print1toN {
+    public static void main(String[] args) {
+        
+        System.out.print("Enter N : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+      
+        print (1,n);
+    }
+    public static void print(int x , int n ){
+        if (x>n) return ;
+        System.out.println(x);
+        print (x+1, n);
+    }
+}
