a3c583e1b7d76196771796850e93898cfe65cc26
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-26 10:48:12 +0530

    Add FunctionCallingItself.java to demonstrate recursion in Java

---
+++ DSA/Recursions/FunctionCallingItself.java
@@ -0,0 +1,13 @@
+package Recursions;
+
+public class FunctionCallingItself {
+    public static void main(String[] args) {
+        ajay(1);
+    }
+
+    public static void ajay (int n )
+{
+    if (n==10) return ;
+    System.out.println("priya");
+    ajay(n+1);
+}}
