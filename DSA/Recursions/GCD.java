a80057490bf17b144c097ae76f5ad24964bc2c92
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-27 15:32:10 +0530

    Add GCD.java to implement a program that calculates the greatest common divisor using recursion

---
+++ DSA/Recursions/GCD.java
@@ -0,0 +1,14 @@
+package Recursions;
+
+public class GCD {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 98;
+        int gcd = findGCD(a, b);
+        System.out.println("GCD of " + a + " and " + b + " is: " + gcd);
+    }
+    public static int findGCD(int a, int b) {
+        if (b == 0) return a;
+        return findGCD(b, a % b);
+    }
+}
