00dd7cb8d624c42a3491263901b84a6d8120bdf4
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-28 23:43:22 +0530

    Add NthStairs.java to implement a program that calculates the number of ways to climb n stairs using recursion

---
+++ DSA/Recursions/NthStairs.java
@@ -0,0 +1,14 @@
+package Recursions;
+
+public class NthStairs {
+    public static void main(String[] args) {
+        int n = 5; // Example: Change this value to test with different inputs
+        int ways = countWays(n);
+        System.out.println("Number of ways to climb " + n + " stairs: " + ways);
+    }
+    public static int countWays(int n) {
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        return countWays(n - 1) + countWays(n - 2);
+    }
+}
