06317faa4a0c3e17ef9dbc18310ba53a67be25ab
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-29 20:50:04 +0530

    Add ReverseArrayUsingRecursion.java to implement array reversal using recursion

---
+++ DSA/Recursions/ReverseArrayUsingRecursion.java
@@ -0,0 +1,14 @@
+package Recursions;
+
+public class ReverseArrayUsingRecursion {
+       public static void main(String[] args) {
+        int[]arr={2,3,5,7,9,12,56,78,54,32};
+        recPrint(arr, arr.length - 1);
+    }
+    public static void recPrint(int[] arr, int idx){
+        
+        if(idx==0) return;
+        System.out.print(arr[idx] + " ");
+        recPrint(arr, idx-1);
+    }
+}
