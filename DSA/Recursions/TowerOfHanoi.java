6ae3221561cd54a47001aa99cc233b6b40cb65ef
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-31 19:29:05 +0530

    Update TowerOfHanoi.java to solve for 4 disks instead of 3

--- DSA/Recursions/TowerOfHanoi.java
+++ DSA/Recursions/TowerOfHanoi.java
@@ -2,16 +2,16 @@ package Recursions;
 
 public class TowerOfHanoi {
     public static void main(String[] args) {
-        hanoi(3, 'A', 'C', 'B');
+        hanoi(4, 'A', 'C', 'B');
     }
     public static void hanoi(int n, char source, char destination, char helper){
         if(n==1){
             System.out.println("Transfer disk " + n + " from " + source + " to " + destination);
             return;
         }
-        hanoi(n-1, source, helper, destination); 
+        hanoi(n-1, source, helper, destination); // move n-1 disks from source to helper via destination
        
-        System.out.println("Transfer disk " + n + " from " + source + " to " + destination);
-        hanoi(n-1, helper, destination, source);
+        System.out.println("Transfer disk " + n + " from " + source + " to " + destination); // move the nth disk from source to destination
+        hanoi(n-1, helper, destination, source); // move n-1 disks from helper to destination via source
     }
 }
