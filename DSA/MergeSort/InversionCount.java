00aef261e9ded386558044bb3deb70d62d7d6a7f
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-03 11:46:06 +0530

    Add Inversion Count implementation with merge sort in InversionCount.java

---
+++ DSA/MergeSort/InversionCount.java
@@ -0,0 +1,50 @@
+package MergeSort;
+
+public class InversionCount {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+        int n = arr.length;
+        int result = mergeSortAndCount(arr, 0, n - 1);
+        System.out.println("Number of inversions are: " + result);
+    }
+    static int mergeAndCount(int[] arr, int left, int mid, int right) {
+        int[] leftArr = new int[mid - left + 1];
+        int[] rightArr = new int[right - mid];
+
+        for (int i = 0; i < leftArr.length; i++)
+            leftArr[i] = arr[left + i];
+        for (int j = 0; j < rightArr.length; j++)
+            rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        int swaps = 0;
+
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+                swaps += (mid + 1) - (left + i);
+            }
+        }
+
+        while (i < leftArr.length)
+            arr[k++] = leftArr[i++];
+
+        while (j < rightArr.length)
+            arr[k++] = rightArr[j++];
+
+        return swaps;
+    }
+
+    static int mergeSortAndCount(int[] arr, int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = (left + right) / 2;
+            count += mergeSortAndCount(arr, left, mid);
+            count += mergeSortAndCount(arr, mid + 1, right);
+            count += mergeAndCount(arr, left, mid, right);
+        }
+        return count;
+    }
+}
