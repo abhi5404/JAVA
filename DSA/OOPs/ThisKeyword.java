efa2f2ffb84aaf4e78070462285ba39d71e3be3b
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-04 17:53:51 +0530

    Add clarification on 'this' keyword usage in Person class constructor

--- DSA/OOPs/ThisKeyword.java
+++ DSA/OOPs/ThisKeyword.java
@@ -25,4 +25,4 @@ class Person {
 
 // This keyword is used to refer to the current object instance of a class. It is commonly used in constructors and methods to distinguish between class attributes and parameters with the same name.
 // In the constructor above, 'this.name' refers to the class attribute, while 'name' refers to the parameter passed to the constructor.
-
+// The same applies to 'this.age' and 'age'.
