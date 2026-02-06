b0cbbc4e4a45f710a9dfa44cbba5b38b438fa418
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-07 01:25:20 +0530

    Implement multiply method in Fraction class and update main for testing

--- DSA/OOPs/Fraction.java
+++ DSA/OOPs/Fraction.java
@@ -14,13 +14,20 @@ public class Fraction {
         num = num * f.den + den * f.num;
         den = den * f.den;
     }
+    void multiply(Fraction f){
+        num = num * f.num;
+        den = den * f.den;
+    }
     public static void main(String[] args) {
         Fraction f1 = new Fraction(3, 4);
         Fraction f2 = new Fraction(5, 6);
         f1.print();
         f2.print();
-        f1.add(f2);
-        System.out.println("After addition:");
+        // f1.add(f2);
+        // System.out.println("After addition:");
+        // f1.print();
+        f1.multiply(f2);
+        System.out.println("After multiplication:");
         f1.print();
     }
 }
