2aa564e14d6d0edb8671a13416608480d304020c
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-07 01:23:45 +0530

    Add Fraction class with methods for addition and printing fractions

---
+++ DSA/OOPs/Fraction.java
@@ -0,0 +1,26 @@
+package OOPs;
+
+public class Fraction {
+    int num;
+    int den;
+    public Fraction(int num, int den) {
+        this.num = num;
+        this.den = den;
+    }
+    void print(){
+        System.out.println(num + "/" + den);
+    }
+    void add(Fraction f){
+        num = num * f.den + den * f.num;
+        den = den * f.den;
+    }
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(3, 4);
+        Fraction f2 = new Fraction(5, 6);
+        f1.print();
+        f2.print();
+        f1.add(f2);
+        System.out.println("After addition:");
+        f1.print();
+    }
+}
