d800a99deb4af6feed51269a30cea9a0265c61c9
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-06 22:10:31 +0530

    Add detailed explanations for getter and setter methods in GetterSetter.java

--- DSA/OOPs/GetterSetter.java
+++ DSA/OOPs/GetterSetter.java
@@ -39,3 +39,5 @@ class Person {
 }
 
 //private variables cannot be accessed directly from outside the class. To access and modify these variables, we use getter and setter methods.
+//A getter method is used to retrieve the value of a private variable, while a setter method is used to set or update the value of a private variable.
+//In the above code, we have a Person class with private variables 'name' and 'age'. We have defined getter and setter methods for both variables, allowing us to access and modify their values from outside the class.
