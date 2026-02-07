8bf79c75e1b7ebee00bfa00d4338c697dbb9e801
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-07 16:31:42 +0530

    Add InsertAtLL class for linked list insertion functionality

---
+++ DSA/LinkedList/InsertAtLL.java
@@ -0,0 +1,41 @@
+package LinkedList;
+class Node {
+    int val;
+    Node next;
+}
+
+public class InsertAtLL {
+      static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    static Node InsertAtHead(int val , Node head){
+        Node temp = new Node();
+        temp.val = val;
+        temp.next = head;
+        head = temp;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(); a.val = 10;
+        Node b = new Node(); b.val = 20;
+        Node c = new Node(); c.val = 30;
+        Node d = new Node(); d.val = 40;
+        Node e = new Node(); e.val = 50;
+        
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        display(a);
+        a = InsertAtHead(5, a);
+        display(a);
+    }
+    
+}
