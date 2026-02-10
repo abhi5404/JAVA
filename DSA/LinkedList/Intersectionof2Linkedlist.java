295e401640fd398f8a48b99b6e00802ce06cb726
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-10 16:55:19 +0530

    Remove main method from IntersectionOf2LinkedList class to streamline functionality

--- DSA/LinkedList/Intersectionof2Linkedlist.java
+++ DSA/LinkedList/Intersectionof2Linkedlist.java
@@ -35,27 +35,5 @@ class IntersectionOf2LinkedList {
         }
         return temp1;
     }
-    public static void main(String[] args) {
-        Node a = new Node(); a.val = 10;
-        Node b = new Node(); b.val = 20;
-        Node c = new Node(); c.val = 30;
-        Node d = new Node(); d.val = 40;
-        Node e = new Node(); e.val = 50;
-        
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-
-        Node x = new Node(); x.val = 100;
-        Node y = new Node(); y.val = 200;
-        Node z = new Node(); z.val = 300;
-
-        x.next = y;
-        y.next = z;
-        z.next = c;
-
-        IntersectionOf2LinkedList obj = new IntersectionOf2LinkedList();
-        System.out.println(obj.intersectPoint(a, x).val);
-    }
+  
 }
