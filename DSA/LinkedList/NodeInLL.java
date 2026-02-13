56dcca73b753c025d4ffce9657dbe1e14a713aad
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-14 00:44:24 +0530

    Refactor Node class in NodeInLL to remove redundant data field and simplify initialization

--- DSA/LinkedList/NodeInLL.java
+++ DSA/LinkedList/NodeInLL.java
@@ -1,25 +1,19 @@
 package LinkedList;
 
-
-    class Node{
-        int val;
-        int data; // alias for val
-        Node next;
-        
-        Node() {}
-        
-        Node(int val) {
-            this.val = val;
-            this.data = val;
-        }
-    }
     public class NodeInLL {
+        static class Node {
+            int val;
+            Node next;
+            Node() {}
+            Node(int val) { this.val = val; }
+        }
+        
         public static void main(String[] args) {
-            Node a = new Node(); a.val=10; a.data=10;
-            Node b = new Node(); b.val=20; b.data=20;
-            Node c = new Node(); c.val=30; c.data=30;
-            Node d = new Node(); d.val=40; d.data=40;
-            Node e = new Node(); e.val=50; e.data=50;
+            Node a = new Node(); a.val=10;
+            Node b = new Node(); b.val=20;
+            Node c = new Node(); c.val=30;
+            Node d = new Node(); d.val=40;
+            Node e = new Node(); e.val=50;
             a.next = b;
             b.next = c;
             c.next = d;
