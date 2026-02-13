53fbb054592101403c47e5f89688c8f9879a630b
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-14 00:43:35 +0530

    Remove Node class definition from ReverseASublist.java to streamline code

--- DSA/LinkedList/ReverseASublist.java
+++ DSA/LinkedList/ReverseASublist.java
@@ -1,15 +1,5 @@
 package LinkedList;
 
-class Node {
-    int data;
-    Node next;
-
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class ReverseASublist {
     public static void main(String[] args) {
         Node head = new Node(1);
