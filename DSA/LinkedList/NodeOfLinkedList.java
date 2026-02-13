cf2cfb7949af11958d1cc7426e3bd140c57c7b2f
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-14 00:42:26 +0530

    Refactor node initialization in DisplayList and NodeOfLinkedList classes for consistency

--- DSA/LinkedList/NodeOfLinkedList.java
+++ DSA/LinkedList/NodeOfLinkedList.java
@@ -3,11 +3,11 @@ package LinkedList;
 public class NodeOfLinkedList {
     public static void main(String[] args) {
         // 10->20->30->40->50
-        Node a = new Node(10); // head node
-        Node b = new Node(20);
-        Node c = new Node(30);
-        Node d = new Node(40);
-        Node e = new Node(50);
+        Node a = new Node(); a.val = 10; // head node
+        Node b = new Node(); b.val = 20;
+        Node c = new Node(); c.val = 30;
+        Node d = new Node(); d.val = 40;
+        Node e = new Node(); e.val = 50;
         // Connect karenge (Link karenge)
         a.next = b;
         b.next = c;
