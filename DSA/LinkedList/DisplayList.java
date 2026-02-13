56fcf85e27dc01f7c2f43a44666bae19392ec7d7
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-14 00:47:19 +0530

    Remove unused linked list classes and related files to streamline the codebase

--- DSA/LinkedList/DisplayList.java
+++ DSA/LinkedList/DisplayList.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 
 public class DisplayList {
-    static class Node {
-        int val;
-        Node next;
-        Node() {}
-        Node(int val) { this.val = val; }
-    }
-    
     public static void displayRec(Node head){
         if(head == null) return;
         displayRec(head.next);
@@ -28,11 +21,11 @@ public class DisplayList {
 //        System.out.println();
     }
     public static void main(String[] args) {
-        Node a = new Node(); a.val = 10; // head node
-        Node b = new Node(); b.val = 200;
-        Node c = new Node(); c.val = 30;
-        Node d = new Node(); d.val = 40;
-        Node e = new Node(); e.val = 50;
+        Node a = new Node(10); // head node
+        Node b = new Node(200);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
         a.next = b; b.next = c;
         c.next = d; d.next = e;
         // display(a);
