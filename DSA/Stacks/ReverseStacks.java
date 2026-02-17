160900edda1cd918564c5a40c0a4f8032955e186
Abhijit Mondal <abhijitamp378@gmail.com>
2026-02-17 23:14:12 +0530

    Add ReverseStacks class to implement stack reversal functionality

---
+++ DSA/Stacks/ReverseStacks.java
@@ -0,0 +1,33 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class ReverseStacks {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10); // bottom
+        st.push(20);
+        st.push(30);
+        st.push(40); // top
+        System.out.println(st);
+        reverse(st);
+        System.out.println(st);
+    }
+    
+    private static void reverse(Stack<Integer> st) {
+        if(st.size()<=1) return;
+        int top = st.pop();
+        reverse(st); // magic
+        pushAtBottom(st,top);
+    }
+
+    private static void pushAtBottom(Stack<Integer> st, int ele) {
+        if(st.size()==0){
+            st.push(ele);
+            return;
+        }
+        int top = st.pop();
+        pushAtBottom(st,ele);
+        st.push(top);
+    }
+}
