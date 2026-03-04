a1f0739ac8b67ca0a43bec1be030a4f2c56ee6eb
Abhijit Mondal <abhijitamp378@gmail.com>
2026-03-04 17:58:36 +0530

    Add implementations for Largest Area in Histogram and Next Greater Element algorithms

---
+++ DSA/Stacks/LargestAreaInHistogram.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    public int largestRectangleArea(int[] arr) {
+
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+
+        // Next Smaller Element
+        int[] nse = new int[n];
+        nse[n - 1] = n;
+        st.push(n - 1);
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            while (st.size() > 0 && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+
+            if (st.size() == 0)
+                nse[i] = n;
+            else
+                nse[i] = st.peek();
+
+            st.push(i);
+        }
+
+        // clear stack
+        while (st.size() > 0)
+            st.pop();
+
+        // Previous Smaller Element
+        int[] pse = new int[n];
+        pse[0] = -1;
+        st.push(0);
+
+        for (int i = 1; i < n; i++) {
+
+            while (st.size() > 0 && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+
+            if (st.size() == 0)
+                pse[i] = -1;
+            else
+                pse[i] = st.peek();
+
+            st.push(i);
+        }
+
+        // calculate max area
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int width = nse[i] - pse[i] - 1;
+            int area = arr[i] * width;
+
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+}
